c15412c040bc75a392ba6bcc3b8f4c1bcc766c5d
Simone Westphal <contact@swestphal.net>
2018-02-04 10:16:14 +0100

    flexi-color-picker integrated and styled

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,59 @@
-const gridActionBtn = document.getElementById( 'btn-generate' );
+const gridActionBtn = document.getElementById('btn-generate');
 
-gridActionBtn.addEventListener( 'click', function ( event ) {
-	event.preventDefault();
-	var gridDim = getSizeOfGrid();
-	generateGrid( gridDim[ 0 ], gridDim[ 1 ] );
-} );
+var pixelColor = '#000';
 
-var getSizeOfGrid = function () {
-	var inputHeight = document.getElementById( 'grid-height' );
-	var inputWidth = document.getElementById( 'grid-width' );
-	var gridHeight = inputHeight.value || inputHeight.placeholder;
-	var gridWidth = inputWidth.value || inputWidth.placeholder;
-	return ( [ gridHeight, gridWidth ] );
+gridActionBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    var gridDim = getSizeOfGrid();
+    generateGrid(gridDim[0], gridDim[1]);
+});
+
+ColorPicker(
+    document.getElementById('colorPicker'),
+    document.getElementById('colorPickerSlide'),
+    function(hex, hsv, rgb) {
+        pixelColor = hex;
+    });
+
+var getSizeOfGrid = function() {
+    var inputHeight = document.getElementById('grid-height');
+    var inputWidth = document.getElementById('grid-width');
+    var gridHeight = inputHeight.value || inputHeight.placeholder;
+    var gridWidth = inputWidth.value || inputWidth.placeholder;
+    return ([gridHeight, gridWidth]);
 };
 
-var generateGrid = function ( gridHeight, gridWidth ) {
-	// create table at grid-table
+var generateGrid = function(gridHeight, gridWidth) {
+    // create table at grid-table
 
-	const tablePos = document.getElementById( 'grid-table' );
-	const oldTable = document.getElementsByTagName( 'table' )[ 0 ];
-	if ( oldTable ) oldTable.remove();
-	var newTable = document.createElement( 'table' );
-	tablePos.appendChild( newTable );
-	for ( var y = 0; y < gridHeight; y++ ) {
-		var newTableRow = document.createElement( 'tr' );
-		newTable.appendChild( newTableRow );
-		for ( var x = 0; x < gridWidth; x++ ) {
-			var newTableCol = document.createElement( 'td' );
-			newTableRow.appendChild( newTableCol );
-		}
-	};
-	paintInGrid();
+    const tablePos = document.getElementById('grid-table');
+    const oldTable = document.getElementsByTagName('table')[0];
+    if (oldTable) oldTable.remove();
+    var newTable = document.createElement('table');
+    tablePos.appendChild(newTable);
+    for (var y = 0; y < gridHeight; y++) {
+        var newTableRow = document.createElement('tr');
+        newTable.appendChild(newTableRow);
+        for (var x = 0; x < gridWidth; x++) {
+            var newTableCol = document.createElement('td');
+            newTableRow.appendChild(newTableCol);
+        }
+    };
+    paintInGrid();
 }
 
-var paintPixel = function ( e ) {
+var paintPixel = function(e) {
 
-	// paint td where mouse is clicked
+    // paint td where mouse is clicked
 
-	//get choosen color
-	const pixelColor = document.getElementById( 'gridColor' ).value;
-	e.target.style.backgroundColor = pixelColor;
+    e.target.style.backgroundColor = pixelColor;
 };
 
-var paintInGrid = function () {
-	// paint with mouseclick in grid
+var paintInGrid = function() {
+    // paint with mouseclick in grid
 
-
-	const tableClick = document.getElementsByTagName( 'table' )[ 0 ];
-	tableClick.addEventListener( 'click', function ( e ) {
-		paintPixel( e );
-	} );
+    const tableClick = document.getElementsByTagName('table')[0];
+    tableClick.addEventListener('click', function(e) {
+        paintPixel(e);
+    });
 };
